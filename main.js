d4e78b86242bf812b89296f2e66cf56443e45f17
Ellis Percival <flyte@failcode.co.uk>
2026-04-14 17:18:28 +0100

    feat: add dbus wrapper and connect on init

--- main.js
+++ main.js
@@ -1,14 +1,25 @@
 const { InstanceBase, runEntrypoint, InstanceStatus } = require('@companion-module/base')
+const { KWinDbus } = require('./lib/dbus')
 
 class KWinDesktopInstance extends InstanceBase {
   async init(config) {
     this.config = config
     this.updateStatus(InstanceStatus.Connecting)
-    this.log('info', 'KWin desktop module initializing')
-    this.updateStatus(InstanceStatus.Ok, 'Scaffold only')
+    this.dbus = new KWinDbus((lvl, msg) => this.log(lvl, msg))
+    try {
+      await this.dbus.connect()
+      const current = await this.dbus.getCurrentDesktop()
+      const count = await this.dbus.getDesktopCount()
+      this.log('info', `KWin connected: desktop ${current}/${count}`)
+      this.updateStatus(InstanceStatus.Ok)
+    } catch (err) {
+      this.log('error', `KWin DBus connect failed: ${err.message}`)
+      this.updateStatus(InstanceStatus.ConnectionFailure, err.message)
+    }
   }
 
   async destroy() {
+    if (this.dbus) this.dbus.disconnect()
     this.log('info', 'KWin desktop module destroyed')
   }
 
